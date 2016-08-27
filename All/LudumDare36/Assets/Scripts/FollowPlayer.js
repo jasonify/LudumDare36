67046a0381d82b2f1a87298d0b1da24449520bd2
jason <bjason@gmail.com>
2016-08-27 01:51:17 -0700

    Adding files

---
+++ All/LudumDare36/Assets/Scripts/FollowPlayer.js
@@ -0,0 +1,16 @@
+﻿var Player : Transform;
+
+var xDist : float = 3;
+var zDist : float = -4;
+
+function Update () {
+	transform.position.z = Player.position.z + zDist;
+	transform.position.x  = Player.position.x + xDist;
+
+}
+
+
+function LastUpdate(){
+	GetComponent.<Camera>().main.transform.position.y = 3;
+}
+
