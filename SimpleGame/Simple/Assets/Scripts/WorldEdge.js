c0f1020ab5ecc79695e5acfb587d77cf2e5804b2
jason <bjason@gmail.com>
2016-08-28 04:12:47 -0700

    Crucial bug fixed

--- SimpleGame/Simple/Assets/Scripts/WorldEdge.js
+++ SimpleGame/Simple/Assets/Scripts/WorldEdge.js
@@ -11,33 +11,54 @@ var PlayerDead : AudioSource;
 
 function OnTriggerEnter(col : Collider) {
 
-
-
-
 	var rb = col.GetComponent.<Rigidbody>();
-	rb.isKinematic = true;
-	yield WaitForSeconds(0.1);
-	rb.isKinematic = false; 
-	//Debug.Log("WorldEdge Hit");
 
-	col.transform.position = Vector3(0, 0.12, 0);
-	if(col.gameObject.tag == "Player"){
-		PlayerDead.Play();
-		col.transform.position = Vector3(-6.75, 0.12, 0.23);	
-		Debug.Log("Game over :(");
-		// TODO: agme over screen
-		GlobalCoins.CoinCount = 0;
+	if(col.gameObject.tag == "DeadPlayer"){
+		
+	} else{
+
+		if(col.gameObject.tag == "Player"){
+
+			yield WaitForSeconds(2);
+			col.gameObject.tag ="DeadPlayer";
+			PlayerDead.Play();
 
 
+			rb.isKinematic = true;
+			yield WaitForSeconds(0.1);
+			rb.isKinematic = false; 
 
-		// Remove all ghosts :( because lost
-		var   gameObjects = GameObject.FindGameObjectsWithTag ("ghost");
-     
-	     for(var i = 0 ; i < gameObjects.Length ; i ++)
-	     {
-	         Destroy(gameObjects[i]);
-	     }
+			col.gameObject.tag ="Player";
 
+
+					col.transform.position = Vector3(-6.75, -0.35, 0.23);	
+			Debug.Log("Game over :(");
+
+			// TODO: agme over screen
+			GlobalCoins.CoinCount = 0;
+
+
+
+			// Remove all ghosts :( because lost
+			var   gameObjects = GameObject.FindGameObjectsWithTag ("ghost");
+	     
+		     for(var i = 0 ; i < gameObjects.Length ; i ++)
+		     {
+		         Destroy(gameObjects[i]);
+		     }
+
+		    
+		} else {
+
+			
+			rb.isKinematic = true;
+			yield WaitForSeconds(0.1);
+			rb.isKinematic = false; 
+			//Debug.Log("WorldEdge Hit");
+
+			col.transform.position = Vector3(0, 0.12, 0);
+
+		}
 	}
 
 }
