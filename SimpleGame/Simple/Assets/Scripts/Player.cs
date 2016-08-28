6f68f1577ab6e8032608ffef6351a63f0ec8e1be
jason <bjason@gmail.com>
2016-08-27 20:02:49 -0700

    Move lil player

---
+++ SimpleGame/Simple/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	public float moveSpeed = 2f;
+	private float maxSpeed = 5f;
+
+	private Vector3 input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3 (-Input.GetAxis ("Horizontal"), 0, -Input.GetAxis ("Vertical"));
+
+		if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed) {
+			GetComponent<Rigidbody>().AddForce (input * moveSpeed);
+		}
+	}
+}
